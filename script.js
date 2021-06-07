dded1a95ce94f7b56132c6f08cf03a14d67f511d
SakriyaDangol <sakriyadangol003@gmail.com>
2021-06-07 20:00:16 +0545

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function loop(n){
-    var text = '';
-    var i;
-    for (i = 1; i <= n; i++) {
-    text += i + '\n';
+    for (let i = 1; i <= n; i++) {
+        if (i%5 == 0 && i%3 == 0) {
+            console.log('fizzbuzz');
+        } else if(i%3 == 0){
+            console.log('fizz');
+        } else if (i%5 == 0) {
+            console.log('buzz'); 
+        } else {
+            console.log(i);
+        }
     }
-    console.log(text);
 }
 
-loop(10);
+loop(15);
